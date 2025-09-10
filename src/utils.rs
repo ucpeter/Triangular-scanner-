1b95b882cc5fa3a30ff9653d8847d3a618680659
ebukapeta <ucpeter10@yahoo.com>
2025-09-10 13:33:45 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,7 +1,10 @@
 use tracing_subscriber::EnvFilter;
 
+pub fn round2(v: f64) -> f64 {
+    (v * 100.0).round() / 100.0
+}
+
 pub fn init_tracing() {
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .init();
+    let filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
+    tracing_subscriber::fmt().with_env_filter(filter).init();
 }
