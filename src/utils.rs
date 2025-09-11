e1c45b3d2d22409822e7b47c6529f8c081ce77fa
ebukapeta <ucpeter10@yahoo.com>
2025-09-11 17:18:48 +0100

    Update utils.rs

--- src/utils.rs
+++ src/utils.rs
@@ -1,12 +1,11 @@
-use tracing_subscriber::{EnvFilter, fmt};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 pub fn init_tracing() {
-    let fmt_layer = fmt::layer().with_target(false);
-    let filter_layer = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
+    let fmt_layer = tracing_subscriber::fmt::layer();
+    let filter_layer = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
     tracing_subscriber::registry()
-        .with(fmt_layer)
         .with(filter_layer)
+        .with(fmt_layer)
         .init();
 }
